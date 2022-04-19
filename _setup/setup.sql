6e752d21478a1d495bf3b23c14055a0f81043f64
Razia <razia.wong@trentglobal.edu.sg>
2022-04-19 06:49:40 +0000

    setup and package

---
+++ _setup/setup.sql
@@ -0,0 +1,3 @@
+use tgc16_poster_shop;
+insert into media_properties (name, description, url) values ('The Beatles', "The Beatles", "https://www.thebeatles.com/"), ('Metallica', "Metallica", "https://www.metallica.com/");
+insert into tags (name) values ("Album"),("Tour"),("Vintage"),("Limited Release");
